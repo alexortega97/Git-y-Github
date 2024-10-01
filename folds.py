250ccac1041da0cc8bfef2fe22e8cf961b8cea43
Alex Ortega <alex.fdo.ortega@gmail.com>
2024-10-01 17:56:00 -0500

    Un comentario

--- folds.py
+++ folds.py
@@ -1,14 +1,1 @@
-import os
-import numpy as np
-from keras.preprocessing.image import load_img, img_to_array
-from sklearn.model_selection import KFold
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.models import Sequential
-import matplotlib.pyplot as plt
-from keras.regularizers import l1
-from tensorflow.keras.callbacks import EarlyStopping
-
-
-
-    
-
+#Solo comentarios
