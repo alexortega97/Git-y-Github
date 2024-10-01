b14f3cf847d900adb8fc7e5996a946eb80a6c626
Alex Ortega <alex.fdo.ortega@gmail.com>
2024-10-01 17:54:02 -0500

    Mas codigo borrado

--- folds.py
+++ folds.py
@@ -42,49 +42,6 @@ print("Forma de las etiquetas:", labels.shape)
 # Inicializar KFold para la validación cruzada con 5 folds
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-# Iterar sobre los folds
-for fold_index, (train_index, val_index) in enumerate(kf.split(images), 1):
-    # Construir el modelo de CNN
-    model = Sequential([
-        Conv2D(64, (3, 3), activation='relu', input_shape=(img_width, img_height, 3)),
-        MaxPooling2D(2, 2),
-        
-        Conv2D(128, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2),
-        
-        #Conv2D(128, (3, 3), activation='relu'),
-        #MaxPooling2D(2, 2),
-        
-       # Conv2D(256, (3, 3), activation='relu'),
-        #MaxPooling2D(2, 2),
-        
-        Flatten(),
-        Dense(32, activation='relu'),# kernel_regularizer=l1(0.0001)),
-        
-        #Dense(32, activation='relu'),# kernel_regularizer=l1(0.0001)),
-        
-        #Dense(16, activation='relu'),# kernel_regularizer=l1(0.0001)),
-        
-        #Dense(8, activation='relu'),# kernel_regularizer=l1(0.0001)),
-        
-        #Dense(4, activation='relu'),# kernel_regularizer=l1(0.0001)),
-        
-        Dense(1, activation='sigmoid')
-    ])
-    
-    # Compilar el modelo
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-    X_train, X_val = images[train_index], images[val_index]
-    y_train, y_val = labels[train_index], labels[val_index]
     
-    # Entrenar el modelo en este fold
-    history = model.fit(X_train, y_train, epochs=400, validation_data=(X_val, y_val), callbacks=[early_stopping])
-    
-    # Guardar el modelo entrenado
-    model.save(f"C:/Users/Usuario/Desktop/temporal/3-64_1-256_{fold_index}.h5")
-    
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    accuracy = history.history['accuracy']
-    val_accuracy = history.history['val_accuracy']
+
